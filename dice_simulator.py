3c877bd332f8a6911b072b6ab449586c491e0716
Gelhpiz Hernandez <gelhpizn@gmail.com>
2026-06-07 20:03:33 -0400

    Revert "style: cambiar formato del mensaje de salida"
    This reverts commit 486aec9185ec7610054e192df18ba23f07c4b8b5.

--- dice_simulator.py
+++ dice_simulator.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     sides = 6
     resultados = lanzar_dados(amount, sides)
     for i, valor in enumerate(resultados, start=1):
-        print(f"Tirada {i} → resultado: {valor}")
+        print(f"Lanzamiento {i} número obtenido {valor}")
         time.sleep(5)
